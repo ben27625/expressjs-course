bd3a4d145f58876704b81901d86aadc3d2254780
sinh <ben.transinh@gmail.com>
2022-11-20 15:03:53 +0700

    mvc

--- src/index.js
+++ src/index.js
@@ -1,16 +1,20 @@
-const { request, Router } = require("express");
-const express = require("express");
-const morgan = require("morgan");
-const path = require("path");
+const { request, Router } = require('express');
+const express = require('express');
+const morgan = require('morgan');
+const path = require('path');
 const app = express();
 const port = 3000;
-const { engine } = require("express-handlebars");
-const { extname } = require("path");
+const { engine } = require('express-handlebars');
+const { extname } = require('path');
+const db = require('./config/db/index');
 
-const route = require('./routes')
+const route = require('./routes');
+
+// connect db
+db.connect();
 
 // static
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 
 route(app);
 
@@ -18,17 +22,17 @@ app.use(express.urlencoded());
 app.use(express.json());
 
 // http loger
-app.use(morgan("combined"));
+app.use(morgan('combined'));
 
 app.engine(
-  "hbs",
+  'hbs',
   engine({
-    extname: ".hbs",
+    extname: '.hbs',
   })
 );
-app.set("view engine", "hbs");
-app.set("views", path.join(__dirname, "/resources/views"));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, '/resources/views'));
 
 app.listen(port, (req, res) => {
-  console.log("App listening on port" + port);
+  console.log('App listening on port' + port);
 });
