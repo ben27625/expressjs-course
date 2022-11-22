2e2a8d6c601fc5f259273a73090b7ba078abc2bf
sinh <ben.transinh@gmail.com>
2022-11-22 14:27:19 +0700

    11:00' create new course

--- src/app/controllers/CourseController.js
+++ src/app/controllers/CourseController.js
@@ -1,19 +1,28 @@
 const { render } = require('node-sass');
 const Course = require('../models/Course');
 
-const {mutipleMongooseToObject, mongooseToObject} = require('../../util/mongoose')
+const {
+  mutipleMongooseToObject,
+  mongooseToObject,
+} = require('../../util/mongoose');
 class CourseController {
   // [GET] /news
- 
-  // [GET] 
-  show(req, res, next) {
-    
 
-   Course.findOne({ slug: req.params.slug}) 
-      .then(course => {
-        res.render('courses/show', {course : mongooseToObject(course)});
+  // [GET]
+  show(req, res, next) {
+    Course.findOne({ slug: req.params.slug })
+      .then((course) => {
+        res.render('courses/show', { course: mongooseToObject(course) });
       })
       .catch(next);
+  }
+
+  create(req, res, next) {
+    res.render('courses/create');
+  }
+
+  store(req, res, next) {
+    res.json(req.body)
   }
 }
 
--- src/routes/courses.js
+++ src/routes/courses.js
@@ -5,6 +5,10 @@ const courseController = require('../app/controllers/CourseController')
 
 // newController.index
 
+router.get('/create' , courseController.create)
+
+router.post('/store' , courseController.store)
+
 
 router.get('/:slug' , courseController.show)
 
